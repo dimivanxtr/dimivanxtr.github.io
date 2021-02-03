d417e7b3921da1d67e1f7586cb904f7eff22aec6
dimivanxtr <78491200+dimivanxtr@users.noreply.github.com>
2021-02-03 16:14:50 +0000

    Cause a redeploy

--- ga-reporter.js
+++ ga-reporter.js
@@ -1,11 +1,10 @@
-const ga_measurement_id='G-47KP9JTKFX'
+const ga_measurement_id = 'G-47KP9JTKFX';
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 ga('create', ga_measurement_id, 'auto');
-
 
 function reportPageLoaded() {
   console.log("page loaded");
@@ -16,6 +15,6 @@ function linkClicked() {
 }
 
 window.addEventListener('load', (event) => {
-    reportPageLoaded();
-    ga('send', 'pageview');
+  reportPageLoaded();
+  ga('send', 'pageview');
 });
