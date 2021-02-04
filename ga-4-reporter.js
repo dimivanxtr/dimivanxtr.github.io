6ced0cc66020761ade95d4c027d6e349c0adb0fd
dimivanxtr <78491200+dimivanxtr@users.noreply.github.com>
2021-02-04 16:48:07 +0000

    Update ga-4-reporter.js

--- ga-4-reporter.js
+++ ga-4-reporter.js
@@ -1,20 +1,15 @@
-const ga_4_measurement_id = 'G-47KP9JTKFX';
+const ga4_measurement_id = 'G-47KP9JTKFX';
 
-let dataLayer = [{
-  'pageCategory': 'StartPage',
-  'visitorType': 'high-value'
-}];
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer', ga_4_measurement_id);
+gtag('config', ga4_measurement_id);
 
 const GA4 = {
   reportPageLoaded: () => {
     console.log("GA4 page loaded");
-    dataLayer.push({'event': 'startPageViewEvent'});
+    gtag('event', 'start_page_visited');
   },
   
   linkClicked: () => {
