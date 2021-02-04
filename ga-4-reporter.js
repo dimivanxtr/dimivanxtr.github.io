eab0beb70d882c5e00665b73624951d1d1f92f12
dimivanxtr <78491200+dimivanxtr@users.noreply.github.com>
2021-02-04 15:03:41 +0000

    Create ga-4-reporter.js

---
+++ ga-4-reporter.js
@@ -0,0 +1,21 @@
+const ga_measurement_id = 'G-47KP9JTKFX';
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer', ga_measurement_id);
+
+/*
+function reportPageLoaded() {
+  console.log("page loaded");
+}
+
+function linkClicked() {
+  console.log("link clicked");
+}
+
+window.addEventListener('load', (event) => {
+  reportPageLoaded();
+  ga('send', 'pageview');
+}); */
